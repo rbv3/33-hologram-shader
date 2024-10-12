5fe4e76caa923012ed712a390076d4a09e0f17a1
Rodrigo Valenca <rodrigo.valenca@ipsos.com>
2024-10-12 18:20:55 -0300

    composing sines to create a more random effect on the glitch

--- src/shaders/holographic/vertex.glsl
+++ src/shaders/holographic/vertex.glsl
@@ -3,9 +3,7 @@ uniform float uTime;
 varying vec3 vModelPosition;
 varying vec3 vNormal;
 
-float random2D(vec2 value) {
-    return fract(sin(dot(value.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
+#include ../includes/random2D.glsl
 
 void main() {
     // Position
@@ -13,7 +11,9 @@ void main() {
     vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
 
     // Glitch
-    float glitchStrength = sin(uTime - modelPosition.y);
+    float glitchTime = uTime - modelPosition.y;
+    float glitchStrength = sin(glitchTime) + sin(glitchTime * 3.45) + sin(glitchTime*8.76);
+    glitchStrength /= 3.0; // since we have 3 sines, we still want the range to be 0-1
     glitchStrength = smoothstep(0.3, 1.0, glitchStrength);
     glitchStrength *= 0.25;
     modelPosition.x += (random2D(modelPosition.xz + uTime) - 0.5) * glitchStrength;
